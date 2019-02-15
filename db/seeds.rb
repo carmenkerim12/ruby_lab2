611c035868ab82898e0baa85114312cb6cb74618
Carmen Kerim <carmen.kerim01@stclairconnect.ca>
2019-02-14 21:02:10 -0500

    Finished lab3

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-(1..3).each do |index|
+(1..24).each do |index|
   user = User.new
-  user.first_name = "carmen #{index}"
-  user.last_name = "Kerim"
-  user.email = "#{index}car@gmail.com"
+  user.first_name = Faker::Name.first_name
+  user.last_name = Faker::Name.last_name
+  user.email = Faker::Internet.email
+  user.street_address = Faker::Address.street_address
+  user.city = Faker::Address.city
+  user.province = Faker::Address.state
+  user.postal_code = Faker::Address.postcode
+  user.country = Faker::Address.country
   user.save
   p "User is created #{user.email}"
-  (1..5).each do |index2|
+  (1..24).each do |index2|
     micropost = Micropost.new
-    micropost.message = "This is a micro post message #{index2}"
+    micropost.message = Faker::Lorem.paragraph
     micropost.user = user
     micropost.save
     p "Micropost is created #{index2}"
